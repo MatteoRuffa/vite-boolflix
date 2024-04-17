3be1a8e50056af44adba5e9b203dc188c3c8e61d
MatteoRuffa <cortese_17@protonmail.com>
2024-04-17 12:20:56 +0200

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  
+  <HeaderComponent />
+  <MainComponent />
 </template>
 
 <script>
 import axios from 'axios';
 import { store } from './store.js';
+import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
   export default {
     name: 'App',
+    components: {
+      HeaderComponent,
+      MainComponent,
+    }
     data() {
       return {
         store
